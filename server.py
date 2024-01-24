940a3d48683c722847e8691b83bd8e738445aa1e
leaps <lmtejedor@gmail.com>
2024-01-24 16:12:13 -0500

    Question + Answer call to replicate

--- server.py
+++ server.py
@@ -1,21 +1,24 @@
 from fastapi import FastAPI, Request, Form, File, UploadFile, HTTPException
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
 from pydantic import BaseModel
 from typing import List, Optional
-from fastapi.responses import HTMLResponse
 import requests
 import replicate
 import os
 from dotenv import load_dotenv
-
 import base64
-import random
 import json
+import io
 
 load_dotenv()
 
 app = FastAPI()
 templates = Jinja2Templates(directory = 'templates')
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 
 os.environ["REPLICATE_API_TOKEN"] = os.getenv("REPLICATE_TOKEN")
@@ -34,3 +37,26 @@ def home(request: Request):
 class Item(BaseModel):
     search: str
 
+@app.post("/")
+async def root(item: Item):
+    print("submit button clicked")
+    test = "test data"
+    
+    image_path = "static/fireplace.jpeg"
+    with open(image_path, "rb") as image_file:
+        encoded_img = base64.b64encode(image_file.read()).decode('utf-8')
+
+    encoded_img = "data:image/jpeg;base64," + encoded_img
+
+    print(encoded_img)
+
+    output = replicate.run(
+        "nateraw/video-llava:a494250c04691c458f57f2f8ef5785f25bc851e0c91fd349995081d4362322dd",
+        input={
+            "image_path": encoded_img,
+            "text_prompt": "What is going on in this image?"
+        }
+    )
+    print(output)
+
+    return {output}
